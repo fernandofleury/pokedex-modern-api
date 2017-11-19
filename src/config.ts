4eb6a7e59c0de98dc2fc0cf5b92d6e15e0f8e5a5
Fernando Fleury Gil <fernandofleuryg@gmail.com>
2017-11-20 00:20:48 +0100

    feat: update seed script

--- src/config.ts
+++ src/config.ts
@@ -4,5 +4,9 @@ dotenv.config();
 export const PORT: string = process.env.PORT || '4000';
 export const GRAPHIQL: boolean = !!process.env.GRAPHIQL;
 export const API: string = process.env.API;
-export const SEED_LOCATION: string = process.env.SEED_LOCATION;
 export const POKEMON_COUNT: number = parseInt(process.env.POKEMON_COUNT, 0);
+export const SEED_LOCATION: string = process.env.SEED_LOCATION;
+export const SEED_REQUEST_DELAY: number = parseInt(
+  process.env.SEED_REQUEST_DELAY,
+  0
+);
--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -1,25 +1,32 @@
-import { SEED_LOCATION, POKEMON_COUNT } from '../config';
+import { SEED_LOCATION, SEED_REQUEST_DELAY, POKEMON_COUNT } from '../config';
 import * as fs from 'fs';
 import * as delay from 'delay';
 import getPokemon from '../repository/getPokemon';
 import range from '../utils/range';
+import { IPokemon } from '../types/pokemon';
 
-const seed = async () => {
-  const results = await Promise.all(
-    range(1, 4).map(async id => {
-      await delay(500);
-      return getPokemon(id);
-    })
-  );
+const makeRequest = async (id: number) => {
+  await delay(SEED_REQUEST_DELAY);
+  process.stdout.write(`generating seed for pokemonId: ${id} \n`);
+  return getPokemon(id);
+};
 
-  const mappedResults = results.reduce((keys, pokemon) => {
-    return {
-      ...keys,
-      [pokemon.id]: pokemon
-    };
-  }, {});
+function* requestGenerator(final: number) {
+  let id = 1;
+  while (id <= final) {
+    yield makeRequest(id++);
+  }
+}
+
+const runSeed = async () => {
+  const mappedResults = {};
+
+  for (const response of requestGenerator(POKEMON_COUNT)) {
+    const result: IPokemon = await response;
+    mappedResults[result.id] = result;
+  }
 
   return fs.writeFileSync(SEED_LOCATION, JSON.stringify(mappedResults), 'utf8');
 };
 
-export default seed();
+export default runSeed();
